f58cd5e247640164df25eaa90a5fd2681ef3fb04
1-21-01-22B-07\pc1-21-01-22B-03 <bryan.corrales@epn.edu.ec>
2018-01-18 10:26:17 -0500

    movimiento solo con las teclas de arriba y abajo

--- README.py
+++ README.py
@@ -2,48 +2,49 @@
 import time
 from tkinter import*
 tk =Tk()
-canvas =Canvas(tk,width=400,height=200)
-canvas1 =Canvas(tk,width=400,height=200)
-canvas2 =Canvas(tk,width=400,height=200)
+posx=200
+posy=200
+canvas =Canvas(tk,width=400,height=400)
 
-
-
-
+img = PhotoImage(file="gol1.png")
+widget = Label(tk, image=img)
 
 def movertri(event):
+    global posx
+    global posy
+
     if event.keysym == 'Up':
         canvas.move(1,0,-3)
-        canvas1.move(1,0,-3)
-        canvas2.move(1,0,-3)
+        posx=posx-3
+        if posx<=110:
+            widget.pack()
+            print("GOL")
+
+        print(posx)
     elif event.keysym == 'Down':
         canvas.move(1,0,3)
-        canvas1.move(1,0,3)
-        canvas2.move(1,0,3)
+        posy=posy+3
+        if posy==220:
+            print("GOL")
+        print(posy)
     elif event.keysym == 'Left':
         canvas.move(1,-3,0)
-        canvas1.move(1,-3,0)
-        canvas2.move(1,-3,0)
+        posx = posx - 3
+        print(posy)
     else:
         canvas.move(1,3,0)
-        canvas1.move(1,3,0)
-        canvas2.move(1,3,0)
+        posy = posy + 3
+        print(posy)
+
+
 canvas.bind_all('<KeyPress-Up>', movertri)
 canvas.bind_all('<KeyPress-Down>', movertri)
-canvas.bind_all('<KeyPress-Left>', movertri)
-canvas.bind_all('<KeyPress-Right>', movertri)
-
-canvas =Canvas(tk,width=400,height=400)
+#canvas.bind_all('<KeyPress-Left>', movertri)
+#canvas.bind_all('<KeyPress-Right>', movertri)
 canvas.pack()
 my_image =PhotoImage(file="pelota.png")
-canvas.create_image(30,10,ancho =NW,image= my_image)
+canvas.create_image(50,200,ancho =NW,image= my_image)
 my_image1 =PhotoImage(file="arco.png")
 canvas.create_image(0,0,ancho =NW,image= my_image1)
+
 tk.mainloop()
-
-
-tk = Tk()
-
-
-
-
-
